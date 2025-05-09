3bac6ac7446870a1912e501f5e96ac5af1785e03
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2025-05-09 17:15:24 +0200

    chore: setup

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	`maven-publish`
+}
+group = "io.github.architectplatform"
+version = "0.1.0"
+
+java {
+	withSourcesJar()
+	withJavadocJar()
+}
+
+publishing {
+	publications {
+		create<MavenPublication>("gpr") {
+			from(components["java"])
+			artifactId = "architect-api"
+			pom {
+				name.set("Architect API")
+				description.set("API for the Architect engine")
+				url.set("https://github.com/architect-platform/architect-api")
+				licenses {
+					license {
+						name.set("Apache-2.0")
+						url.set("https://www.apache.org/licenses/MIT-2.0")
+					}
+				}
+				developers {
+					developer {
+						id.set("alemazzo")
+						name.set("Alessandro Mazzoli")
+					}
+				}
+			}
+		}
+	}
+	repositories {
+		maven {
+			name = "GitHubPackages"
+			url = uri("https://maven.pkg.github.com/architect-platform/architect-api")
+			credentials {
+				username = project.findProperty("githubUser") as String?
+				password = project.findProperty("githubToken") as String?
+			}
+		}
+	}
+}
+
+
