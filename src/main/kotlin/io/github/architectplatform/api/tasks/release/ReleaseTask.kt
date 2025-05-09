2f0bc77b14d7ba7d949787075bad7980c94313d6
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2025-05-10 01:25:02 +0200

    fix: typo

--- src/main/kotlin/io/github/architectplatform/api/tasks/release/ReleaseTask.kt
+++ src/main/kotlin/io/github/architectplatform/api/tasks/release/ReleaseTask.kt
@@ -3,5 +3,5 @@ package io.github.architectplatform.api.tasks.release
 import io.github.architectplatform.api.tasks.Task
 import io.github.architectplatform.api.tasks.publish.PublishTaskResult
 
-interface ReleaseTask : Task<PublishTaskResult>
+interface ReleaseTask : Task<ReleaseTaskResult>
 
